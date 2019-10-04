bfb398bacbfe4e855bc778d6560c49a0b1b8a321
Andy Bettisworth <andy@accreu.com>
2019-10-04 11:18:11 -0500

    Solve for One-Way Flight Trip problem

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,13 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    for i in range(0, length):
+        if i == 0:
+            route[i] = hash_table_retrieve(hashtable, 'NONE')
+        else:
+            route[i] = hash_table_retrieve(hashtable, route[i-1])
 
     return route
